68e4c110c08ac66d84f34ad64e10754f4c71d7c7
peytia <parchiba@calpoly.edu>
2023-02-28 12:59:14 -0800

    eafeafae

---
+++ src/mainpage.py
@@ -0,0 +1,13 @@
+'''!
+    @file mainpage.py
+    @brief                      Functions as a mainpage file to run with doxygen
+    @details                    This file will be processed by doxygen to create a main page for navigation and explanitory
+                                purposes for the .
+
+    @author                     Peyton Archibald
+    @author                     Harrison Hirsch
+    @date                       February 28, 2023
+
+
+
+'''
