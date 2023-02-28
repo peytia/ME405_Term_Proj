587dedf757c8b6e4824c56cbef4ac4dc9f68a298
peytia <parchiba@calpoly.edu>
2023-02-28 13:09:23 -0800

    no comment

--- src/mainpage.py
+++ src/mainpage.py
@@ -1,13 +1,18 @@
 '''!
-    @file mainpage.py
-    @brief                      Functions as a mainpage file to run with doxygen
-    @details                    This file will be processed by doxygen to create a main page for navigation and explanitory
-                                purposes for the term project.
+    @file       mainpage.py
+    @brief      Functions as a mainpage file to run with doxygen
+    @details    This file will be processed by doxygen to create a main page for navigation and explanitory
+                purposes for the term project.
 
-    @author                     Peyton Archibald
-    @author                     Harrison Hirsch
-    @date                       February 28, 2023
+    @author     Peyton Archibald
+    @author     Harrison Hirsch
+    @date       February 28, 2023
 
+    @section    Software Design
 
-
+    @subsection Yaw Motor Task
+    @subsection Pitch Motor Task
+    @subsection Fire Control Task
+    @subsection User Input Task
+    @subsection Camera Task
 '''
